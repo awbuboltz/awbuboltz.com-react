6655b492dccd63cf5a0161b47ca504ac162f92d5
andy <andy@awbuboltz.com>
2018-03-29 19:28:06 -0500

    Use default props

--- src/components/SocialLinks.js
+++ src/components/SocialLinks.js
@@ -5,12 +5,14 @@ import Anchor from './Anchor';
 export class SocialLinks extends Component {
 
     render() {
-        const links = this.props.socialLinks
-            ? this.props.socialLinks.map((linkData, i) => {
-                    return <Anchor {...linkData} key={i} />;
-                })
-            : [];
+        const links = this.props.socialLinks.map((linkData, i) => {
+            return <Anchor {...linkData} key={i} />;
+        });
 
         return (<p>{links}</p>);
     }
 }
+
+SocialLinks.defaultPrpops = {
+    socialLinks: []
+};
