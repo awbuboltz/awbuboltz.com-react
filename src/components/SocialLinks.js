be3469eb789a09b5f4d19f44fa4156925d3ca82a
andy <andy@awbuboltz.com>
2018-03-31 21:47:57 -0500

    Specify SocialLink params

--- src/components/SocialLinks.js
+++ src/components/SocialLinks.js
@@ -6,7 +6,11 @@ export class SocialLinks extends Component {
 
     render() {
         const socialLinks = this.props.socialLinks.map((linkData, i) => {
-            return <SocialLink {...linkData} key={i} />;
+            return <SocialLink
+                key={i}
+                icon={linkData.icon}
+                text={linkData.text}
+                url={linkData.url} />;
         });
 
         return (<span>{socialLinks}</span>);
